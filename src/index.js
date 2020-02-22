dbf6a776a5cc0a78af8fb1afd9f6bdf33899c03f
Horácio Pedro <46960527+horacio-pedro@users.noreply.github.com>
2020-02-22 19:06:01 +0100

    commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path')
+const cors = require('cors')
 
 mongoose.connect('mongodb://localhost/so07backend', {
 	useNewUrlParser: true,
@@ -8,6 +9,14 @@ mongoose.connect('mongodb://localhost/so07backend', {
 })
 
 const app = express();
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+app.use((req, res) => {
+	req.io = io
+})
+
+app.use(cors())
 
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resize')))
 app.use(require('./routes'))
